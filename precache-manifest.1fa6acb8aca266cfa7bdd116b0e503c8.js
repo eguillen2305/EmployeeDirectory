d8fd04ec5deb001dd992dd9916c3e718fc28683d
Eduardo Guillen <eguillen2035@gmail.com>
2020-09-04 15:51:03 -0500

    Updates

---
+++ precache-manifest.1fa6acb8aca266cfa7bdd116b0e503c8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c8810d3a5e2d7fb633d5b6920bfe0c5",
+    "url": "/EmployeeDirectory/index.html"
+  },
+  {
+    "revision": "6f507b163acb883fcd54",
+    "url": "/EmployeeDirectory/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "e5d3905e097dabaf463b",
+    "url": "/EmployeeDirectory/static/css/main.da86aa9d.chunk.css"
+  },
+  {
+    "revision": "6f507b163acb883fcd54",
+    "url": "/EmployeeDirectory/static/js/2.3fe2fc9e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/EmployeeDirectory/static/js/2.3fe2fc9e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e5d3905e097dabaf463b",
+    "url": "/EmployeeDirectory/static/js/main.d3c6e877.chunk.js"
+  },
+  {
+    "revision": "86db9ffb54bbcd2be0ab",
+    "url": "/EmployeeDirectory/static/js/runtime-main.e529978e.js"
+  }
+]);
--- src/setupTests.js
+++
@@ -1,3 +0,0 @@
-import '@testing-library/jest-dom/extend-expect';
-
-//jest matchers for asserting on DOM nodes.
